1bf29dbc79b7e11bdc7d973fdb0f8d821c55d378
Tatsuya28 <ioiodauphin880@gmail.com>
2024-06-13 16:30:09 +0200

    Setting and securization of TCP client and server

---
+++ tcp-client.c
@@ -0,0 +1,92 @@
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 500
+
+void usage(const char *progname) {
+    printf("%s -p <port_id> -h <serveur_ip>\n", progname);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+    char buffer[BUFFER_SIZE+1];
+    struct sockaddr_in remote_address;
+    int sock_to_server;
+
+    // Setting up getopt
+    char *host;
+    int port;
+    int opt = 0;
+
+    while ((opt = getopt(argc, argv, "p:h:")) != -1) {
+        switch (opt) {
+            case 'p':
+                port = atoi(optarg);
+                printf("Defined port: %d\n", port);
+                break;
+            case 'h':
+                host = optarg;
+                printf("Defined host: %s\n", host);
+                break;
+            default:
+                usage(argv[0]);
+        }
+    }
+
+    if (host == NULL || port == 0) {
+        usage(argv[0]);
+    }
+
+    // Create socket
+    sock_to_server = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_to_server < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure remote address
+    remote_address.sin_family = AF_INET;
+    remote_address.sin_port = htons(port);
+    if (inet_pton(AF_INET, host, &(remote_address.sin_addr)) <= 0) {
+        perror("Invalid address/Address not supported");
+        close(sock_to_server);
+        exit(EXIT_FAILURE);
+    }
+    memset(&(remote_address.sin_zero), 0, sizeof(remote_address.sin_zero));
+
+    // Connect to server
+    if (connect(sock_to_server, (struct sockaddr *) &remote_address, sizeof(struct sockaddr_in)) < 0) {
+        perror("Connection to the server failed");
+        close(sock_to_server);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to %s on port %d\n", host, port);
+
+    while (1) {
+        printf("Enter your message: ");
+        fflush(stdout);
+        size_t bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        if (bytes_read < 0) {
+            perror("Read failed");
+            close(sock_to_server);
+            exit(EXIT_FAILURE);
+        }
+        buffer[bytes_read] = '\0';
+        size_t bytes_sent = send(sock_to_server, buffer, bytes_read+1, 0);
+        if (bytes_sent < 0) {
+            perror("Send failed");
+            close(sock_to_server);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    close(sock_to_server);
+    return 0;
+}
---
+++ tcp-server.c
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 500
+
+void usage(const char *progname) {
+    printf("%s -p <port_id> -h <serveur_ip>\n", progname);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+    char buffer[BUFFER_SIZE+1];
+    struct sockaddr_in my_address;
+    int srv_socket, client_socket;
+
+    // Setting up getopt
+    char *host;
+    int port;
+    int opt = 0;
+
+    while ((opt = getopt(argc, argv, "p:h:")) != -1) {
+        switch (opt) {
+            case 'p':
+                port = atoi(optarg);
+                printf("Defined port: %d\n", port);
+                break;
+            case 'h':
+                host = optarg;
+                printf("Defined host: %s\n", host);
+                break;
+            default:
+                usage(argv[0]);
+        }
+    }
+
+    if (host == NULL || port == 0) {
+        usage(argv[0]);
+    }
+
+    // Create socket
+    srv_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (srv_socket < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address
+    my_address.sin_family = AF_INET;
+    if (inet_pton(AF_INET, host, &my_address.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        close(srv_socket);
+        exit(EXIT_FAILURE);
+    }
+    my_address.sin_port = htons(port);
+    memset(&(my_address.sin_zero), 0, sizeof(my_address.sin_zero));
+    
+    // Bind socket to the specified port
+    if (bind(srv_socket, (struct sockaddr *) &my_address, sizeof(struct sockaddr_in)) < 0) {
+        perror("Bind failed");
+        close(srv_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incomming connections
+    if (listen(srv_socket, 10) < 0) {
+        perror("Listen failed");
+        close(srv_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", port);
+
+    // Accept incomming connections
+    while (1) {
+        client_socket = accept(srv_socket, NULL, NULL);
+        if (client_socket < 0) {
+            perror("Accept failed");
+            close(srv_socket);
+            exit(EXIT_FAILURE);
+        }
+        printf("Client connected\n");
+
+        // Read and print messages from the client
+        ssize_t bytes_received;
+        while ((bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0)) > 0) {
+            buffer[bytes_received] = '\0';
+            printf("Received: %s\n", buffer);
+        }
+        
+        if (bytes_received < 0) {
+            perror("Receive failed");
+        }
+
+        printf("Client disconnected\n");
+        close(client_socket);
+    }
+
+    close(srv_socket);
+    return 0;
+}
